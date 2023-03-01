59e038950013d68bf1028437333aa447a8dfb5dd
Nick_Linux <n.avramenko87@gmail.com>
2023-03-02 00:12:49 +0300

    add new metods / add tests

--- src/classes.py
+++ src/classes.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class Storage(ABC):
@@ -6,17 +6,31 @@ class Storage(ABC):
 
     def __init__(self, items: dict):
         self.items = items
-        self.capacity = 100
+        self.capacity = 200
 
     def add(self, description: str, count: int):
+        # check item for availability and space
+
+        # add new item and check space
         if self.items.get(description, False) is False:
-            self.items[description] = count
+            if count >= self.get_free_space():
+                print("No free space")
+            else:
+                self.items[description] = count
+        # add existing item and check space
         else:
-            self.items[description] = self.items[description] + count
+            if self.items[description] + count >= self.get_free_space():
+                print("No free space")
+            else:
+                self.items[description] = self.items[description] + count
+
 
     def remove(self, description: str, count: int):
+
+        # not found item
         if self.items.get(description, False) is False:
             print("No this items")
+        # found item delete need counts
         else:
             if self.items[description] - count <= 0:
                 del self.items[description]
@@ -24,23 +38,50 @@ class Storage(ABC):
                 self.items[description] = self.items[description] - count
 
     def get_free_space(self):
+        all_space = self.capacity
+        busy_free_space = 0
         for item in self.items.values():
-            self.capacity -= item
-        return self.capacity
+            busy_free_space += item
+
+        return all_space - busy_free_space
 
     def get_items(self):
         return self.items
 
     def get_unique_items_count(self):
-        unique_items = []
-        for item in self.items:
+        unique_items = 0
+        for item, value in self.items.items():
             if "уникальн" in item.lower():
-                unique_items.append(item)
-            return unique_items
+                unique_items += value
+        return unique_items
 
 
 class Store(Storage):
-    capacity = 100
-
+    def __init__(self, items: dict):
+        super().__init__(items)
+        self.capacity = 100
 class Shop(Storage):
-    capacity = 20
+
+    def __init__(self, items: dict):
+        super().__init__(items)
+        self.capacity = 20
+
+    def add(self, description: str, count: int):
+        # check item for availability and space
+
+        # add new item and check space
+        if self.items.get(description, False) is False:
+            if count >= self.capacity:
+                print("No free space")
+            elif len(self.items) > 5:
+                print("Different items more than 5!")
+            else:
+                self.items[description] = count
+        # add existing item and check space
+        else:
+            if self.items[description] + count >= self.capacity:
+                print("No free space")
+            elif len(self.items) > 5:
+                print("Different items more than 5!")
+            else:
+                self.items[description] = self.items[description] + count
--- src/utils.py
+++ src/utils.py
@@ -3,11 +3,34 @@ from classes import *
 
 data = {}
 test = Store(data)
-test.add("sprite унИкальный", 1)
+
+test.add("sprite унИкальный", 3)
+test.add("sprite унИкальный", 9)
+test.add("cola", 11)
 test.add("cola", 10)
-test.add("cola", 1)
-test.remove("cola", 111)
-print(test.items)
 print(test.get_free_space())
 print(test.get_items())
-print(test.get_unique_items_count())
+
+#test2 = Shop(data2)
+#data2 = {}
+# print("-----")
+# print(test2.capacity)
+# test2.add("cola", 2)
+# test2.add("cola2", 1)
+# test2.add("cola3", 1)
+# test2.add("cola4", 1)
+# test2.add("cola77", 19)
+# test2.add("cola771", 1)
+# print(test2.items)
+
+r = {}
+
+
+# def get_free_space(items):
+#     capacity = 100
+#     for item in items.values():
+#         print(item)
+#         capacity -= item
+#     return capacity
+#
+# print(get_free_space(r))
